12ac69d8653bd0a5242e2c7751638c0167b351a0
afaslah <afaslah12@gmail.com>
2026-03-10 08:49:42 +0700

    feat: update loading spinner style

--- src/components/common/LoadingSpinner.jsx
+++ src/components/common/LoadingSpinner.jsx
@@ -1,12 +1,16 @@
+import { CircularProgress, Box } from "@mui/material";
+
 export default function LoadingSpinner() {
   return (
-    <div style={{ 
-      display: 'flex', 
-      justifyContent: 'center', 
-      alignItems: 'center', 
-      minHeight: '200px' 
-    }}>
-      <div>Loading...</div>
-    </div>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "200px",
+      }}
+    >
+      <CircularProgress size={50} thickness={4} sx={{ color: "#FF8040" }} />
+    </Box>
   );
 }
